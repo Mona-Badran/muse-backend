6a75f037e5ded112ffc4e7d655ec116751e070cf
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-13 01:18:02 +0200

    added forgot password controller to auth module

--- src/modules/auth/controller.js
+++ src/modules/auth/controller.js
@@ -1,3 +1,4 @@
 export const login = async (req, res) => {};
 export const register = async (req, res) => {};
 export const logout = async (req, res) => {};
+export const forgotPassword = async (req, res) => {};
