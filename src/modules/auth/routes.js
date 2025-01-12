6223e9a0c25071f9280c6186fdb9f5e2fe5a64c3
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-13 01:39:03 +0200

    added forgot password route to auth module

--- src/modules/auth/routes.js
+++ src/modules/auth/routes.js
@@ -6,3 +6,4 @@ const authRouter = new Router();
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
 authRouter.post("/register", register);
+authRouter.post("/password", forgotPassword);
