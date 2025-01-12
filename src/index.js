234850de979b5552d34ad954284d6ef42ceaf9de
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-13 01:04:18 +0200

    Initialized Express app with configuration setup in init.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
-import cors from 'cors';
 import dotenv from 'dotenv';
+import { init } from './config/init.js';
 import connectToDatabase from './db/connection.js';
 
 const app = express();
 dotenv.config();
+
+init(app);
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server running on port ${process.env.SERVER_PORT}`);
