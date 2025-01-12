c3922396ffa364df41166c002fc615b0c6bf6b02
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-13 00:56:27 +0200

    Added database connection initialization in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectToDatabase from './db/connection.js';
 
 const app = express();
 dotenv.config();
@@ -14,4 +15,5 @@ app.use(
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server running on port ${process.env.SERVER_PORT}`);
+    connectToDatabase();
 });
