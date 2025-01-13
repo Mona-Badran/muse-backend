895781356c6a14eecdb545c32fcbec57ce451709
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-13 02:14:26 +0200

    registered authRouter in the app using registerRoutes function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from 'dotenv';
-import { init } from './config/init.js';
+import { init, registerRoutes} from './config/init.js';
 import connectToDatabase from './db/connection.js';
+import uathRouter from './modules/auth/routes.js';
 
 const app = express();
 dotenv.config();
 
 init(app);
+registerRoutes(app, uathRouter);
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server running on port ${process.env.SERVER_PORT}`);
