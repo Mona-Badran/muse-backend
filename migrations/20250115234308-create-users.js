3b1b949535b79bbb9bab5b02524f65579e8024e6
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-16 01:50:45 +0200

    added created_at and updated_at fields to users migration with non-null constraints and default values

--- migrations/20250115234308-create-users.js
+++ migrations/20250115234308-create-users.js
@@ -28,6 +28,16 @@ module.exports = {
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE',
       },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
     });
 
   },
