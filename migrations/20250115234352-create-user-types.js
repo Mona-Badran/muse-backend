553093a9cdb0eac0ff375caa5e61c8e349287f46
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-16 01:54:48 +0200

    added user_types migration with createTable command

--- migrations/20250115234352-create-user-types.js
+++ migrations/20250115234352-create-user-types.js
@@ -3,20 +3,16 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('user_types', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    
   }
 };
