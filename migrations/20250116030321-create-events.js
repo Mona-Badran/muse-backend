bc98ce511d3389ad0883d8e785de833684375dcc
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-16 05:04:53 +0200

    added createTable definition for events migration

--- migrations/20250116030321-create-events.js
+++ migrations/20250116030321-create-events.js
@@ -1,6 +1,8 @@
 export default {
   async up (queryInterface, Sequelize) {
-    
+    await queryInterface.createTable('events', {
+      
+    });
   },
 
   async down (queryInterface, Sequelize) {
