f2ce9f7c67d273644757c4cd1b41c8bff22c77a5
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-16 02:58:56 +0200

    added user_type_id field to User model with non-null constraint

--- models/users.js
+++ models/users.js
@@ -11,6 +11,10 @@ module.exports = (sequelize, DataTypes) => {
                 type: DataTypes.STRING,
                 allowNull: false,
             },
+            user_type_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
         },
     );
     return User;
