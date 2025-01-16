effacf8102353864a4db6601e58cca9640a3dc90
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-16 05:01:14 +0200

    added primary key definition to sites table migration

--- migrations/20250116025651-create-sites.js
+++ migrations/20250116025651-create-sites.js
@@ -1,7 +1,11 @@
 export default {
   async up (queryInterface, Sequelize) {
     await queryInterface.createTable('sites', {
-      
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
     });
 
   },
