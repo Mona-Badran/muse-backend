d7d8f22393c196a34abfc499bb2e475419ec9a6e
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-16 06:31:42 +0200

    added name attribute to ArtGallery model definition

--- models/art_gallery.js
+++ models/art_gallery.js
@@ -2,7 +2,11 @@ export default (sequelize, DataTypes) => {
     const ArtGallery = sequelize.define(
         'ArtGallery',
         {
-            
+            name: 
+            {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
         },
     );
 };
