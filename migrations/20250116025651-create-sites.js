673987a6f8285221f12ef485c48614d701ad03be
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-16 05:02:12 +0200

    added user_id field with foreign key reference to users table in sites migration

--- migrations/20250116025651-create-sites.js
+++ migrations/20250116025651-create-sites.js
@@ -19,6 +19,15 @@ export default {
       longitude: {
         type: Sequelize.FLOAT,
       },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
     });
 
   },
