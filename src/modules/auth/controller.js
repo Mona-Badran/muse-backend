bfa39093eb7607e6f4bc4a5d0d450d1d62b5cc70
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-16 06:49:20 +0200

    implemented login function with error handling in auth controller

--- src/modules/auth/controller.js
+++ src/modules/auth/controller.js
@@ -2,7 +2,14 @@ import { User } from "../../models/user.js";
 import jwt from "jsonwebtoken";
 import { handlePassword } from "./service.js";
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
 export const register = async (req, res) => {};
 export const logout = async (req, res) => {};
 export const forgotPassword = async (req, res) => {};
