95317c5a57fdae3c82ffe64a89c38ee36a66217e
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-16 06:29:17 +0200

    added user_id attribute to Attend model

--- models/attend.js
+++ models/attend.js
@@ -2,7 +2,11 @@ export default (sequelize, DataTypes) => {
     const Attend = sequelize.define(
         'Attend',
         {
-            
+            user_id: 
+            {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
         },
     );
 };
