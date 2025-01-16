8cf5a4289ff454b5b4a94d2866f3ed14de7398d3
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-16 05:01:24 +0200

    added name field to sites table migration

--- migrations/20250116025651-create-sites.js
+++ migrations/20250116025651-create-sites.js
@@ -6,6 +6,10 @@ export default {
         autoIncrement: true,
         primaryKey: true,
       },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
     });
 
   },
