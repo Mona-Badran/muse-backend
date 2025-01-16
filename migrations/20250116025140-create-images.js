cc5215bc5cb878baf244d02eac0c376246be877c
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-16 04:54:49 +0200

    added id and image_url fields to create-images migration

--- migrations/20250116025140-create-images.js
+++ migrations/20250116025140-create-images.js
@@ -1,7 +1,15 @@
 export default {
   async up (queryInterface, Sequelize) {
     await queryInterface.createTable('images', {
-      
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      image_url: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
     });
   },
 
