f781d412d1c2a2fc4e260153a240698351370dee
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-16 05:05:04 +0200

    added primary key definition for events table migration

--- migrations/20250116030321-create-events.js
+++ migrations/20250116030321-create-events.js
@@ -1,7 +1,11 @@
 export default {
   async up (queryInterface, Sequelize) {
     await queryInterface.createTable('events', {
-      
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
     });
   },
 
