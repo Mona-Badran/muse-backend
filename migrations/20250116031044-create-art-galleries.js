6de458e5ab0fc02064e42532b734e37cf941f221
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-16 05:11:57 +0200

    added createTable statement for art_galleries migration

--- migrations/20250116031044-create-art-galleries.js
+++ migrations/20250116031044-create-art-galleries.js
@@ -1,6 +1,8 @@
 export default {
   async up (queryInterface, Sequelize) {
-    
+    await queryInterface.createTable('art_galleries', {
+      
+    });
   },
 
   async down (queryInterface, Sequelize) {
