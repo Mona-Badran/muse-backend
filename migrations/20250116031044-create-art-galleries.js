c4817d93f42c437e60362203f5f5f72614c5932b
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-16 05:12:53 +0200

    added owner_id column to art_galleries table migration with foreign key reference to users

--- migrations/20250116031044-create-art-galleries.js
+++ migrations/20250116031044-create-art-galleries.js
@@ -16,6 +16,15 @@ export default {
       images: {
         type: Sequelize.TEXT,
       },
+      owner_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
     });
   },
 
