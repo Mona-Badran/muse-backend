96ac581e4433427879b25df9e8968e1ab783d492
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-16 03:01:30 +0200

    added association between User and UserType models

--- models/users.js
+++ models/users.js
@@ -22,5 +22,12 @@ module.exports = (sequelize, DataTypes) => {
             underscored: true,
         }
     );
+    User.associate = (models) => {
+        User.belongsTo(models.UserType, {
+            foreignKey: 'user_type_id',
+            as: 'user_type',
+        });
+    };
+    
     return User;
 };
