2133ccf7ff80c91c6131858833800491ff511318
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-16 06:06:21 +0200

    added name attribute to Event model with validation

--- models/event.js
+++ models/event.js
@@ -2,7 +2,11 @@ export default (sequelize, DataTypes) => {
     const Event = sequelize.define(
         'Event',
         {
-            
+            name: 
+            {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
         },
     );
     return Event;
