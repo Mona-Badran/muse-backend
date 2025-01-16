b03062e8b4507741b562026954486dd152974db6
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-16 05:08:43 +0200

    added user_id column with foreign key constraints to attend table migration

--- migrations/20250116030720-create-attend.js
+++ migrations/20250116030720-create-attend.js
@@ -1,7 +1,15 @@
 export default {
   async up (queryInterface, Sequelize) {
     await queryInterface.createTable('attend', {
-      
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
     });
   },
 
