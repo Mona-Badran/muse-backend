544ec78cfdb323150ac2d5186dfe848aa1756268
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-16 02:58:06 +0200

    added username field to User model with uniqueness constraint

--- models/users.js
+++ models/users.js
@@ -2,7 +2,11 @@ module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define(
         'User',
         {
-
+            username: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+            },
         },
     );
     return User;
