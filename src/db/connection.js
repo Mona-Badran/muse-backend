de71d15a98df0b8bfbc5f0d663b426a49487166e
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-17 03:36:49 +0200

    renamed database environment variables for consistency in connection settings

--- src/db/connection.js
+++ src/db/connection.js
@@ -7,11 +7,12 @@ const connectToDatabase = async () => {
     try {
         const connection = await mysql.createConnection({
             host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASS,
+            user: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
         });
         console.log('Database connected');
+
         return connection;
     }
     catch (error) {
