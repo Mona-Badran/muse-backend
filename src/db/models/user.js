16c77aae6ad3f1983405d5c78a5157358994f9fe
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-17 14:00:46 +0200

    changed the names of user and usertype

---
+++ src/db/models/user.js
@@ -0,0 +1,39 @@
+export default (sequelize, DataTypes) => {
+    const User = sequelize.define(
+        'User',
+        {
+            id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+            username: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            user_type_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+        },
+        {
+            tableName: 'users',
+            timestamps: true,
+            underscored: true,
+        }
+    );
+
+    User.associate = (models) => {
+        User.belongsTo(models.UserType, {
+            foreignKey: 'user_type_id',
+            as: 'user_type',
+        });
+    };
+    
+    return User;
+};
---
+++ src/db/models/user_type.js
@@ -0,0 +1,23 @@
+export default (sequelize, DataTypes) => {
+    const UserType = sequelize.define(
+        'UserType',
+        {
+            user_type: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+        },
+        {
+            tableName: 'user_types',
+            timestamps: true,
+            underscored: true,
+        }
+    );
+    UserType.associate = (models) => {
+        UserType.hasMany(models.User, {
+            foreignKey: 'user_type_id',
+            as: 'users',
+        });
+    };
+    return UserType;
+};
