08284d43b0e75f1dbdbaea7e8067cdfebe73d4ff
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-18 20:20:22 +0200

    added GET route for retrieving events by ID in galleryRouter

--- src/modules/gallery/routes.js
+++ src/modules/gallery/routes.js
@@ -6,3 +6,4 @@ const galleryRouter = new Router();
 galleryRouter.get("/", getGalleries);
 galleryRouter.get("/search", gallerySearch);
 galleryRouter.get("/:id", getGallery);
+galleryRouter.get("/event/:id", getEvent);
