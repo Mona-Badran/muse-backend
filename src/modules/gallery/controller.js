0dbe4136f1a0540f73928c66cb085a92dcec2344
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-18 20:22:29 +0200

    added createEvent and updateimages functions to gallery controller

--- src/modules/gallery/controller.js
+++ src/modules/gallery/controller.js
@@ -2,3 +2,5 @@ export const gallerySearch = async (req, res) => { };
 export const getGalleries = async (req, res) => { };
 export const getGallery = async (req, res) => { };
 export const getEvent = async (req, res) => { };
+export const createEvent = async (req, res) => { };
+export const updateimages = async (req, res) => { };
