8458ea444d014d80aca261ea669db2b7828aa13a
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-18 20:29:07 +0200

    added error handling to gallerySearch function

--- src/modules/gallery/controller.js
+++ src/modules/gallery/controller.js
@@ -2,7 +2,14 @@ import db from "../../db/models/index.js";
 const { ArtGallery } = db;
 const { Event } = db;
 
-export const gallerySearch = async (req, res) => { };
+export const gallerySearch = async (req, res) => { 
+    try {
+        
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send({ message: error.message });
+    }
+};
 export const getGalleries = async (req, res) => { };
 export const getGallery = async (req, res) => { };
 export const getEvent = async (req, res) => { };
