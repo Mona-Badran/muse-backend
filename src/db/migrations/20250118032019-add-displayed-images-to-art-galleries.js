302ff41f909cc65fa25529a97671e07c3edc5f60
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-18 05:24:24 +0200

    added displayed_images column to art_galleries migration

--- src/db/migrations/20250118032019-add-displayed-images-to-art-galleries.js
+++ src/db/migrations/20250118032019-add-displayed-images-to-art-galleries.js
@@ -1,9 +1,11 @@
 export default {
   async up (queryInterface, Sequelize) {
-
+    await queryInterface.addColumn('art_galleries', 'displayed_images', {
+      
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    
+
   }
 };
