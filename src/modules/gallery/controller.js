1b94a8aa0b6527e8a0177a407eeba6bc828bf7b9
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-18 20:11:29 +0200

    added getEvent function to gallery controller

--- src/modules/gallery/controller.js
+++ src/modules/gallery/controller.js
@@ -1,3 +1,4 @@
 export const gallerySearch = async (req, res) => { };
 export const getGalleries = async (req, res) => { };
 export const getGallery = async (req, res) => { };
+export const getEvent = async (req, res) => { };
