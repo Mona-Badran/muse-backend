284c0a673e26110fdf79baf47ce2b1660ab69798
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-22 00:09:15 +0200

    removed unused file system checks for uploads directory in artwork service

--- src/modules/artwork/artwork.service.js
+++ src/modules/artwork/artwork.service.js
@@ -1,12 +1,5 @@
 import multer from "multer";
 import path from "path";
-import fs from "fs";
-
-const uploads = path.join(process.cwd(), "src", "modules", "artwork", "uploads");
-
-if (!fs.existsSync(uploads)) {
-    fs.mkdirSync(uploads, { recursive: true });
-}
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
