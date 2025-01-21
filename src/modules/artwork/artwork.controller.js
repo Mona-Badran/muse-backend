213b3ccd25fe2d05bbdaac8ce506fa18ae190a81
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-21 06:51:37 +0200

    added image entry creation in getImage controller

--- src/modules/artwork/artwork.controller.js
+++ src/modules/artwork/artwork.controller.js
@@ -11,6 +11,13 @@ export const getImage = async (req, res) => {
             return res.status(400).send({ message: "User ID is required" });
         }
         const imageUrl = await uploadImage(req, res);
+        
+        const imageEntry = await Image.create({
+            image_url: imageUrl,
+            user_id: user_id,
+            ML_result_id: ML_result_id || null,
+        });
+
         return res.status(201).send({
             message: "Image uploaded successfully",
             imageUrl: imageUrl,
