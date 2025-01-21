67e61b8b6da774ba389d88cbe4d6f859c67c149e
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-22 00:07:55 +0200

    updated upload destination path in artwork service to use absolute path

--- src/modules/artwork/artwork.service.js
+++ src/modules/artwork/artwork.service.js
@@ -10,7 +10,7 @@ if (!fs.existsSync(uploads)) {
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads/");
+        cb(null, path.join(process.cwd(), 'src/modules/artwork/uploads'));
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${file.originalname}`);
