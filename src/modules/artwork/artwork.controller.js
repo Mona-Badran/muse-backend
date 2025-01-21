7369df74663e7e7584c485597ee40623d5216fd8
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-21 05:23:46 +0200

    implemented uploadImage function

--- src/modules/artwork/artwork.controller.js
+++ src/modules/artwork/artwork.controller.js
@@ -1,3 +1,13 @@
 import db from "../../db/models/index.js";
 
-export const uploadImage = async (req, res) => {};
+export const uploadImage = async (req, res) => {
+    try {
+        const imageUrl = await uploadImage(req, res);
+        return res.status(201).send({
+            message: "Image uploaded successfully",
+            imageUrl: imageUrl,
+        });
+    } catch (error) {
+        return res.status(400).send({ message: error });
+    }
+};
