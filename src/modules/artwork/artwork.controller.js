ff9407a69620d3fa7e2d28d8f59f8da39e11ab47
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-22 00:07:00 +0200

    refactored uploadArtwork function to use saveImage and improve error handling

--- src/modules/artwork/artwork.controller.js
+++ src/modules/artwork/artwork.controller.js
@@ -3,30 +3,14 @@ import { saveImage } from "./artwork.service.js";
 
 const { Image } = db;
 
-export const uploadArtwork = async (req, res) => {};
-
-export const getImage = async (req, res) => {
+export const uploadArtwork = async (req, res) => {
     try {
-        const { user_id, ML_result_id } = req.body;
-
-        user_id = parseInt(req.body.user_id, 10);
-
-        if (!user_id) {
-            return res.status(400).send({ message: "User ID is required" });
-        }
-        const imageUrl = await uploadImage(req, res);
-        
-        const imageEntry = await Image.create({
-            image_url: imageUrl,
-            user_id: user_id,
-            ML_result_id: ML_result_id || null,
-        });
-
-        return res.status(201).send({
-            message: "Image uploaded and saved to database successfully",
-            imageUrl: imageEntry,
+        const imageUrl = await saveImage(req, res);
+        res.status(200).json({
+            message: 'Image uploaded successfully',
+            imageUrl: imageUrl,
         });
     } catch (error) {
-        return res.status(400).send({ message: error });
+        res.status(400).json({ message: error.message });
     }
 };
