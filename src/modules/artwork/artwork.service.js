35c18d3ad0033ad160a600de4421b656e13e6691
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-21 05:23:05 +0200

    handled image uploads and return image URL

--- src/modules/artwork/artwork.service.js
+++ src/modules/artwork/artwork.service.js
@@ -12,3 +12,17 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage }).single("image");
 
+export const uploadImage = (req, res) => {
+    return new Promise((resolve, reject) => {
+        upload(req, res, (err) => {
+            if (err) {
+                reject("Error uploading image");
+            } else if (!req.file) {
+                reject("No image provided");
+            } else {
+                const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+                resolve(imageUrl);
+            }
+        });
+    });
+};
