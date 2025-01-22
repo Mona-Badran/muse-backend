1171ea397d95c304bc04511e069638cdac068bc7
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-22 02:19:26 +0200

    added primary key to Site model

--- src/db/models/site.js
+++ src/db/models/site.js
@@ -2,6 +2,11 @@ export default (sequelize, DataTypes) => {
     const Site = sequelize.define(
         'Site',
         {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
             name:
             {
                 type: DataTypes.STRING,
